d41425c1a98aab17ec6707cc0f9364be679dad31
M.Kunde <mikolaj.kundegorski@gmail.com>
2017-01-31 13:07:15 +0000

    spacemacs and opencv

--- installopencv.sh
+++ installopencv.sh
@@ -10,13 +10,13 @@ git clone https://github.com/opencv/opencv.git
  cd opencv/
  mkdir release
  cd release/
- cmake -D CMAKE_BUILD_TYPE=Release -D OPENCV_BUILD_3RDPARTY_LIBS=1 -D CMAKE_CXX_FLAGS="-march=native" -D OPENCV_EXTRA_MODULES_PATH="../../opencv_contrib/modules" -D CMAKE_INSTALL_PREFIX=/opt/opencv-3-dev ..
+ cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_CXX_FLAGS="-march=native" -D OPENCV_EXTRA_MODULES_PATH="../../opencv_contrib/modules" -D CMAKE_INSTALL_PREFIX=/opt/opencv-3-dev ..
  make -j 8
  sudo make install
  cd ..
  mkdir debug
  cd debug/
- cmake -D CMAKE_BUILD_TYPE=Debug -D OPENCV_BUILD_3RDPARTY_LIBS=1 -D CMAKE_CXX_FLAGS="-march=native" -D OPENCV_EXTRA_MODULES_PATH="../../opencv_contrib/modules" -D CMAKE_INSTALL_PREFIX=/opt/opencv-3-dev-dbg ..
+ cmake -D CMAKE_BUILD_TYPE=Debug -D CMAKE_CXX_FLAGS="-march=native" -D OPENCV_EXTRA_MODULES_PATH="../../opencv_contrib/modules" -D CMAKE_INSTALL_PREFIX=/opt/opencv-3-dev-dbg ..
  make -j 8
  sudo make install
  cd ../../../
