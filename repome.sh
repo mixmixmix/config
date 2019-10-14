09cbcba1235a267262cc269d3eb358e6f3fb0306
mixmixmix <mikolaj.kundegorski@gmail.com>
2019-10-14 16:54:28 +0100

    more repos

--- repome.sh
+++ repome.sh
@@ -1,39 +1,7 @@
 #!/usr/bin/env zsh
 set -xe
-mkdir repos
+if [ ! -d repos ]; then
+	mkdir repos
+fi
 cd repos
-git clone git@bitbucket.org:mkundegorski/organutan.git
-git clone git@bitbucket.org:mkundegorski/latex.git
-git clone git@bitbucket.org:mkundegorski/rachael.git
-git clone git@bitbucket.org:mkundegorski/deveron.git
-git clone git@bitbucket.org:mkundegorski/notes.git
-git clone git@bitbucket.org:mkundegorski/tex-cv.git
-git clone git@bitbucket.org:mkundegorski/pdf-works.git
-git clone git@bitbucket.org:mkundegorski/flask4sci.git
-git clone git@bitbucket.org:mkundegorski/rapportron-skeleton.git
-git clone git@bitbucket.org:mkundegorski/meerkat-setup.git
-git clone git@bitbucket.org:mkundegorski/meerkat.git
-git clone git@bitbucket.org:mkundegorski/alistarsedge.git
-git clone git@bitbucket.org:mkundegorski/classificationpeopleir.git
-git clone git@bitbucket.org:mkundegorski/msapient.git
-git clone git@bitbucket.org:mkundegorski/bgm_harness.git
-git clone git@bitbucket.org:mkundegorski/sapient.git
-git clone git@bitbucket.org:mkundegorski/visapient.git
-git clone git@bitbucket.org:mkundegorski/universallearning.git
-git clone git@bitbucket.org:mkundegorski/configrepo.git
-git clone git@bitbucket.org:mkundegorski/hogmatch.git
-git clone git@bitbucket.org:mkundegorski/variouslittleprograms.git
-git clone git@bitbucket.org:mkundegorski/advancedbehaviour.git
-git clone git@bitbucket.org:mkundegorski/multithread.git
-git clone git@bitbucket.org:mkundegorski/behave.git
-git clone git@bitbucket.org:mkundegorski/visposture.git
-git clone git@bitbucket.org:mkundegorski/sapient_phase2.git
-git clone git@bitbucket.org:mkundegorski/wwwmiks.git
-git clone git@bitbucket.org:mkundegorski/bg_study.git
-git clone git@bitbucket.org:mkundegorski/baggage.git
-git clone git@bitbucket.org:mkundegorski/terramax.git
-git clone git@bitbucket.org:mkundegorski/suikate2.git
-git clone git@bitbucket.org:mkundegorski/lovelace.git
-git clone git@bitbucket.org:mkundegorski/surikate.git
-git clone git@bitbucket.org:mkundegorski/turquoise.git
-git clone git@bitbucket.org:mkundegorski/skyblue.git
+git clone git@github.com:fjelltopp/meerkat_dev.git
---
+++ repome_github.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+set -xe
+if [ ! -d repos ]; then
+	mkdir repos
+fi
+cd repos
+git clone git@github.com:fjelltopp/meerkat_dev.git
+git clone git@github.com:mixmixmix/config.git
+git clone git@github.com:mixmixmix/moreyfirth.git
+git clone git@github.com:mixmixmix/parrtraks.git
+git clone git@github.com:mixmixmix/thesis.git
+
+
