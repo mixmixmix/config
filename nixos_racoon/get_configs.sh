1ae6a765c818296dfff81635cdc57fae205230b1
M Kunde <mikolaj.kundegorski@gmail.com>
2020-04-02 23:56:52 +0100

    common nix

--- nixos_racoon/get_configs.sh
+++ nixos_racoon/get_configs.sh
@@ -1,9 +1,10 @@
+cp /etc/nixos/common_mixpacks.nix ../
+cp ~/.xmonad/xmonad.hs ../
+cp /etc/nixos/pythonix.nix ../
+cp ~/.xmobarrc ../xmobarrc
+cp ~/.spacemacs ../spacemacs
+
 cp /etc/nixos/configuration.nix .
-cp /etc/nixos/common_mixpacks.nix .
-cp /etc/nixos/pythonix.nix .
-cp ~/.xmonad/xmonad.hs .
-cp ~/.spacemacs spacemacs
-cp ~/.xmobarrc xmobarrc
 cp ~/onstart.sh onstart.sh
 echo "copied configs"
 git add .
---
+++ update-nix.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo cp common_mixpacks.nix /etc/nixos/
+sudo cp pythonix.nix /etc/nixos/
+
+echo "Finished!"
