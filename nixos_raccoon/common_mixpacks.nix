5104b7de0629fc6340abaa02fe8845f5c6686c11
mixmixmix <mikolaj.kundegorski@gmail.com>
2020-04-27 22:09:25 +0100

    dateutils

--- nixos_raccoon/common_mixpacks.nix
+++ nixos_raccoon/common_mixpacks.nix
@@ -21,7 +21,7 @@ in
     oh-my-zsh usbutils mc irssi sl exfat-utils 	gnupg archiver 	bzip2 unzip 	chrony pmutils 	geteltorito 	colordiff 	arp-scan
     fortune cowsay lolcat autofs5 afuse
     pciutils #lspci
-    chrony gparted ntfs3g
+    chrony gparted ntfs3g dateutils
     #file sync
     dropbox dropbox-cli
     bitwarden bitwarden-cli
