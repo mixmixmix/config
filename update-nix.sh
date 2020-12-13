9f340aeffc25da8a47100a17ae2d6bed6ee75a69
mixmixmix <mikolaj.kundegorski@gmail.com>
2020-12-13 17:16:25 +0600

    ignore bups, common ssh setup

--- update-nix.sh
+++ update-nix.sh
@@ -5,7 +5,11 @@ echo "You are on ${HOST}"
 sudo cp common_mixpacks.nix /etc/nixos/
 sudo cp common_configuration.nix /etc/nixos/
 sudo cp pythonix.nix /etc/nixos/
+cp ~/.spacemacs ~/spacemacs.old
 cp ~/.spacemacs spacemacs.old
+cp ~/.ssh/config ~/.ssh/sshconf.old
+cp ~/.ssh/config sshconf.old
+cp sshconf ~/.ssh/config
 cp spacemacs ~/.spacemacs
 cd nixos_${HOST}
 sudo cp configuration.nix /etc/nixos/
