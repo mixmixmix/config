d921c513467ca01c6b2c6c3e70c12c9f1e51cf38
mixmixmix <mikolaj.kundegorski@gmail.com>
2021-03-24 16:01:18 +0000

    cleanup

--- update-nix.sh
+++ update-nix.sh
@@ -6,13 +6,14 @@ sudo cp common_mixpacks.nix /etc/nixos/
 sudo cp common_configuration.nix /etc/nixos/
 sudo cp pythonix.nix /etc/nixos/
 cp ~/.spacemacs ~/spacemacs.old
-cp ~/.spacemacs spacemacs.old
+cp ~/.spacemacs autobup/spacemacs.old
 cp ~/.ssh/config ~/.ssh/sshconf.old
-cp ~/.ssh/config sshconf.old
-cp sshconf ~/.ssh/config
-cp tmux.conf ~/.tmux.conf
-cp spacemacs ~/.spacemacs
-cp xmonad.hs ~/.xmonad/xmonad.hs
+cp ~/.ssh/config autobup/sshconf.old
+cp appconfig/sshconf ~/.ssh/config
+cp appconfig/tmux.conf ~/.tmux.conf
+cp appconfig/spacemacs ~/.spacemacs
+cp -R appconfig/ranger ~/.config/
+cp appconfig/xmonad.hs ~/.xmonad/xmonad.hs
 xmonad --recompile
 cd nixos_${HOST}
 sudo cp configuration.nix /etc/nixos/
