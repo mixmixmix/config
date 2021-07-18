84035b8c4e27844c9a1ccf89702e1c995a9ee4d1
M Kunde <mikolaj.kundegorski@gmail.com>
2021-07-18 09:13:53 +0100

    ignore spacemacs settings for now

--- update-nix.sh
+++ update-nix.sh
@@ -5,13 +5,13 @@ echo "You are on ${HOST}"
 sudo cp common_mixpacks.nix /etc/nixos/
 sudo cp common_configuration.nix /etc/nixos/
 sudo cp pythonix.nix /etc/nixos/
-cp ~/.spacemacs ~/spacemacs.old
-cp ~/.spacemacs autobup/spacemacs.old
+#cp ~/.spacemacs ~/spacemacs.old
+#cp ~/.spacemacs autobup/spacemacs.old
 cp ~/.ssh/config ~/.ssh/sshconf.old
 cp ~/.ssh/config autobup/sshconf.old
 cp appconfigs/sshconf ~/.ssh/config
 cp appconfigs/tmux.conf ~/.tmux.conf
-cp appconfigs/spacemacs ~/.spacemacs
+#cp appconfigs/spacemacs ~/.spacemacs
 cp -R appconfigs/ranger ~/.config/
 cp appconfigs/xmonad.hs ~/.xmonad/xmonad.hs
 xmonad --recompile
